356fb29341a44aed25bb7ec67e464e0f8a20f3c9
Cy Kirsch <cykirsch@gmail.com>
2018-07-01 21:30:40 -0500

    Implement basically working operations of the RPN program including error handling for input of unaccepted values, as well as operators input when there are no operands to operate on

--- src/Calculator/RPN.php
+++ src/Calculator/RPN.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace Calculator;
+namespace Cyk\Calculator;
+
+use Cyk\Exception\InvalidInputException;
 
 class RPN
 {
@@ -10,23 +12,80 @@ class RPN
     const OPERATOR_DIVIDE = '/';
     // TODO support alternatives like x and ÷
 
+    private $result;
+    private $operand_stack;
+
     public function __construct()
     {
-        echo 'construct';
+        $this->reset();
     }
 
     public function reset()
     {
-        echo 'reset';
+        $this->result = null;
+        $this->operand_stack = [];
     }
 
-    public function operand($operand)
+    public function operand(string $operand)
     {
-        echo $operand;
+        if ($this->isValidOperand($operand)) {
+            $operand = round($operand, 10);
+            array_push($this->operand_stack, $operand);
+        }
+
+        return $operand;
     }
 
-    public function operator($operator)
+    public function operator(string $operator)
     {
-        echo $operator;
+        if ($this->isEnoughOperands()) {
+            if ($this->result === null) {
+                $this->result = array_pop($this->operand_stack);
+            }
+
+            $operand = array_pop($this->operand_stack);
+            switch($operator) {
+                case self::OPERATOR_ADD:        $this->result += $operand; break;
+                case self::OPERATOR_SUBTRACT:   $this->result -= $operand; break;
+                case self::OPERATOR_MULTIPLY:   $this->result *= $operand; break;
+                case self::OPERATOR_DIVIDE:     $this->result /= $operand; break;
+            }
+        }
+
+        return round($this->result, 10);
+    }
+
+    /**
+     * Must be at least one operand on the stack before accepting an operator;
+     * if this is the first operator in the calculation, must be at least two operands.
+     * @return boolean Only if true/valid
+     * @throws Exception\InvalidInputException
+     */
+    private function isEnoughOperands() : bool
+    {
+        if ($this->result === null && count($this->operand_stack) < 2) {
+            throw new InvalidInputException("Cannot accept an operator before there are at least two operands.");
+        }
+
+        if ($this->result !== null && count($this->operand_stack) < 1) {
+            throw new InvalidInputException("Need at least one more operand before accepting an operator.");
+        }
+
+        return true;
+    }
+
+    /**
+     * Determine if the given operand is valid. Must be numeric. As far as I am aware nothing else matters.
+     * @return boolean Only if true/valid
+     * @throws Exception\InvalidInputException
+     */
+    private function isValidOperand($operand) : bool
+    {
+        if (!is_numeric($operand)) {
+            $current = $this->result === null ? 'empty' : (string) $this->result;
+            throw new InvalidInputException("'$operand' is not a valid arithmetic operand. The current result is still $current.");
+        }
+
+        return true;
     }
 }
--- src/rpn-cli.php
+++ src/rpn-cli.php
@@ -2,34 +2,39 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Calculator\RPN;
+use Cyk\Calculator\RPN;
+use Cyk\Exception\InvalidInputException;
 
 $rpn = new RPN();
-// $rpn->reset();
 
 // Continually request new input and operate on it
 while (true) {
+    echo "> ";
     $input = strtoupper(trim(fgets(STDIN)));
 
-    switch ($input) {
-        case 'Q':
-        case 'QUIT':
-        case 'EXIT':
-            exit("\n");
+    try {
+        switch ($input) {
+            case "\n":
+            case "\r":
+            case "\r\n":
+                continue;
 
-        case RPN::OPERATOR_ADD:
-        case RPN::OPERATOR_SUBTRACT:
-        case RPN::OPERATOR_MULTIPLY:
-        case RPN::OPERATOR_DIVIDE:
-            echo $rpn->operator($input);
-            break;
+            case 'Q':
+            case 'QUIT':
+            case 'EXIT':
+                exit("\n");
 
-        default:
-            try {
-                echo $rpn->operand($input);
-            } catch (Exception $e) {
-                // TODO more specific exception
-                echo $e->getMessage();
-            }
+            case RPN::OPERATOR_ADD:
+            case RPN::OPERATOR_SUBTRACT:
+            case RPN::OPERATOR_MULTIPLY:
+            case RPN::OPERATOR_DIVIDE:
+                echo $rpn->operator($input) . "\n";
+                break;
+
+            default:
+                    echo $rpn->operand($input) . "\n";
+        }
+    } catch (InvalidInputException $e) {
+        echo $e->getMessage() . "\n";
     }
 }
