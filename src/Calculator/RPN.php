0edbd641cbf0497bf598e9b4c0224ed656c5234f
Cy Kirsch <cykirsch@gmail.com>
2018-07-01 19:30:22 -0500

    Setup project structure

---
+++ src/Calculator/RPN.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Calculator;
+
+class RPN
+{
+    public function __construct()
+    {
+        echo 'construct';
+    }
+
+    public function reset()
+    {
+        echo 'reset';
+    }
+}
